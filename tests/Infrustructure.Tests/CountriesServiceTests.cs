309c54422e67e07e880c8ce64ca512086bfcf277
Serhii Konovalov <skonova@softserveinc.com>
2023-08-22 01:19:46 +0300

    cover CountriesService with unit tests

---
+++ tests/Infrustructure.Tests/CountriesServiceTests.cs
@@ -0,0 +1,74 @@
+using Infrastructure.Options;
+using Infrastructure.Services;
+using Microsoft.Extensions.Options;
+using Moq;
+using System.Net;
+using System.Text.Json;
+
+namespace Infrastructure.Tests;
+
+public class CountriesServiceTests
+{
+    private readonly Mock<IOptions<CountriesApiOptions>> _optionsMock;
+    private readonly Mock<IHttpClientFactory> _httpClientFactoryMock;
+    private const string ApiUrl = "http://testapi.com";
+
+    public CountriesServiceTests()
+    {
+        _optionsMock = new Mock<IOptions<CountriesApiOptions>>();
+        _optionsMock.Setup(o => o.Value).Returns(new CountriesApiOptions { ApiUrl = ApiUrl });
+
+        _httpClientFactoryMock = new Mock<IHttpClientFactory>();
+    }
+
+    private CountriesService CreateServiceWithHandler(DelegatingHandler handler)
+    {
+        var httpClient = new HttpClient(handler);
+        _httpClientFactoryMock.Setup(c => c.CreateClient(It.IsAny<string>())).Returns(httpClient);
+        return new CountriesService(_httpClientFactoryMock.Object, _optionsMock.Object);
+    }
+
+    [Fact]
+    public async Task FetchCountriesAsync_ShouldReturnCountries_WhenApiResponseIsValid()
+    {
+        // Arrange
+        var clientHandlerStub = new DelegatingHandlerStub((request, cancellationToken) =>
+        {
+            var content = File.ReadAllText("../../../TestData/full-response-valid.json");
+            return new HttpResponseMessage()
+            {
+                StatusCode = HttpStatusCode.OK,
+                Content = new StringContent(content)
+            };
+        });
+        var service = CreateServiceWithHandler(clientHandlerStub);
+
+        // Act
+        var countries = await service.FetchCountriesAsync();
+
+        // Assert
+        Assert.NotNull(countries);
+    }
+
+    [Fact]
+    public async Task FetchCountriesAsync_ThrowsJsonException_WhenApiProvidesInvalidJson()
+    {
+        // Arrange
+        var clientHandlerStub = new DelegatingHandlerStub((request, cancellationToken) =>
+        {
+            var content = File.ReadAllText("../../../TestData/partial-response-invalid.json");
+            return new HttpResponseMessage()
+            {
+                StatusCode = HttpStatusCode.OK,
+                Content = new StringContent(content)
+            };
+        });
+
+        var httpClient = new HttpClient(clientHandlerStub);
+        var httpClientFactoryMock = new Mock<IHttpClientFactory>();
+        httpClientFactoryMock.Setup(c => c.CreateClient(It.IsAny<string>())).Returns(httpClient);
+
+        // Act & Assert
+        await Assert.ThrowsAsync<JsonException>(() => CreateServiceWithHandler(clientHandlerStub).FetchCountriesAsync());
+    }
+}
---
+++ tests/Infrustructure.Tests/DelegatingHandlerStub.cs
@@ -0,0 +1,16 @@
+﻿namespace Infrastructure.Tests;
+
+public class DelegatingHandlerStub : DelegatingHandler
+{
+    private readonly Func<HttpRequestMessage, CancellationToken, HttpResponseMessage> _handlerFunc;
+
+    public DelegatingHandlerStub(Func<HttpRequestMessage, CancellationToken, HttpResponseMessage> handlerFunc)
+    {
+        _handlerFunc = handlerFunc ?? throw new ArgumentNullException(nameof(handlerFunc));
+    }
+
+    protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+    {
+        return Task.FromResult(_handlerFunc(request, cancellationToken));
+    }
+}
