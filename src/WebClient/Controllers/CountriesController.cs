5cae176562204b92ac0daae4938c1f84653d6f6a
Serhii Konovalov <skonova@softserveinc.com>
2023-08-21 20:59:44 +0300

    implement restcountries service requesting

---
+++ src/WebClient/Controllers/CountriesController.cs
@@ -0,0 +1,26 @@
+﻿using Infrastructure.Models;
+using Infrastructure.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebClient.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CountriesController : ControllerBase
+    {
+        private readonly ICountriesService _countriesService;
+
+        public CountriesController(ICountriesService countriesService)
+        {
+            _countriesService = countriesService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IReadOnlyCollection<Country>>> Get([FromQuery] string? param1, [FromQuery] int? param2, [FromQuery] string? param3, [FromQuery] string? param4)
+        {
+            var countries = await _countriesService.FetchCountriesAsync();
+
+            return Ok(countries);
+        }
+    }
+}
--- src/WebClient/Program.cs
+++ src/WebClient/Program.cs
@@ -1,7 +1,11 @@
+using Infrastructure.Options;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+builder.Services.AddHttpClient();
+builder.Services.AddCountriesService(builder.Configuration);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
